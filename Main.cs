50deed3a2fa04266b6644bb413f02cd7726eb897
Shape581 <severusrogue5008@gmail.com>
2025-02-18 23:31:53 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,20 @@
+﻿using Life;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logs581
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            //Coucou
+        }
+    }
+}
